7cd72cab0f188b46c9b816f579987eac51ac34c0
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-12-13 16:52:42 -0800

    update for 3D plot

--- matlot3Dtest.py
+++ matlot3Dtest.py
@@ -6,70 +6,45 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
-def _3dplot1():
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-
-    x = [1,2,3,4,5]
-    y = [2,4,6,8,10]
-    z = np.linspace(0,100,11)
-    Y,Z = np.meshgrid(y,z)
-    X = np.array([x]*Y.shape[0])
-
-    ax.plot_surface(X, Y, Z)
-    plt.show()
-
 def _3dplotLine():
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-    z = np.linspace(-2, 2, 100)
-    r = z**2 + 1
-    x = r * np.sin(theta)
-    y = r * np.cos(theta)
+    t = np.linspace(0, 10, 101)
+    x = t
+    y = t
+    z = -t
     ax.plot(x, y, z)
-    ax.legend()
-
     plt.show()
-
-def randrange(n, vmin, vmax):
-    return (vmax - vmin)*np.random.rand(n) + vmin
-
-
 def _3dplotScatter():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-
-    n = 100
-    for c, m, zlow, zhigh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
-        xs = randrange(n, 23, 32)
-        ys = randrange(n, 0, 100)
-        zs = randrange(n, zlow, zhigh)
-        ax.scatter(xs, ys, zs, c=c, marker=m)
+    csvFile = np.loadtxt('exampledata.csv', delimiter=',', skiprows=1)
+    NumberOfRow = csvFile.shape[0]
+    rowRange = range (NumberOfRow)
+    for indexRow in rowRange:
+        [xs,ys,zs] = csvFile[indexRow]
+        print([xs,ys,zs])
+        ax.scatter(xs, ys, zs, c = 'y')
     plt.show()
 
 def _3dplotSurface():
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    X = np.arange(-5, 5, 0.25)
-    Y = np.arange(-5, 5, 0.25)
+    X = np.arange(0, 2, 1)
+    Y = np.arange(0, 2, 1)
     X, Y = np.meshgrid(X, Y)
-    R = np.sqrt(X**2 + Y**2)
-    Z = np.sin(R)
+    Z = -X -Y
+    print(X)
+    print(Y)
+    print(Z)
 
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
-
-    ax.set_zlim(-1.01, 1.01)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-
+    ax.plot_surface(X, Y, Z)
     plt.show()
 
 def main():
+    _3dplotLine()
+    _3dplotScatter()
     _3dplotSurface()
 
 if __name__ == "__main__":
